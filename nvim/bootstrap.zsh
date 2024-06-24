5b17f10ae87f413000c2e2ddbca5cd61af125861
Arnau Mas <arnau.mas@outlook.com>
2024-06-24 10:44:21 +0200

    separate initialization scripts into different files

--- nvim/bootstrap.zsh
+++ nvim/bootstrap.zsh
@@ -4,5 +4,3 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Symlink configuration files
-ln -s "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
---
+++ nvim/link.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# Symlink configuration files
+ln -s "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -s "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
