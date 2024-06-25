be8cb2daea14b59bbb68b5a9edf71eea661fab07
Arnau Mas <arnau.mas@outlook.com>
2024-06-25 10:31:52 +0200

    move nord configuration to lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+-- load lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -5,6 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- load plugins
 require("lazy").setup({
 	spec = {
 		"L3MON4D3/LuaSnip",
@@ -14,5 +16,16 @@ require("lazy").setup({
 	install = { colorscheme = { "nord"} }
 })
 
+-- configure nord colorscheme
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "nord",
+	callback = function()
+		vim.cmd.highlight({ "Search", "guibg=#81a1c1" })
+		vim.cmd.highlight({ "CurSearch", "guifg=#2e3440", "guibg=#Ebcb8b" })
+		vim.cmd.highlight({ "IncSearch", "guifg=#2e3440", "guibg=#Ebcb8b" })
+		vim.cmd.highlight({ "Comment", "guifg=#b8bec7" })
+	end,
+	desc = "override highlight colors for search hits and comments",
+})
 vim.g.nord_disable_background = true
 require('nord').set()
