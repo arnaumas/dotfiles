e8724e41283cd4952337f0432be50c9b4c064ddf
Arnau Mas <arnau.mas@outlook.com>
2024-06-25 11:08:19 +0200

    initial luasnip installation

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,3 +47,17 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 })
 vim.g.nord_disable_background = true
 require('nord').set()
+
+-- configure luasnip
+local ls = require("luasnip")
+vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
+
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/luasnip/"})
--- nvim/link.zsh
+++ nvim/link.zsh
@@ -2,3 +2,4 @@
 
 # Symlink configuration files
 ln -s "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/dotfiles/nvim/luasnip" "$HOME/.config/nvim"
