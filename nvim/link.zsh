76fee511f514522c018393e633ca1ea73279208c
Arnau Mas <arnau.mas@outlook.com>
2024-06-27 16:17:25 +0200

    revert to vimscript configuration, install vim-plug

--- nvim/link.zsh
+++ nvim/link.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
 # Symlink configuration files
-ln -s "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+# ln -s "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+ln -s "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 mkdir -p "$HOME/.config/nvim/luasnip"
 ln -s "$HOME/dotfiles/nvim/luasnip/all.lua" "$HOME/.config/nvim/luasnip/all.lua"
+
+sh -c 'curl -fLo "$HOME/.config/nvim/autoload/plug.vim" --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
