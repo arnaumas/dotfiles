1504cd289df5597d477a2ac2b1b09b2c8967839a
Arnau Mas <arnau.mas@outlook.com>
2025-01-30 12:44:12 +0100

    packer.nvim installed

--- nvim/link.zsh
+++ nvim/link.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Symlink configuration files
-ln -s "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -s "$HOME/dotfiles/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 
 mkdir -p "$HOME/.config/nvim/lua"
 ln -s "$HOME/dotfiles/nvim/lua/packer.lua" "$HOME/.config/nvim/lua/packer.lua"
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,3 +4,5 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  end)
