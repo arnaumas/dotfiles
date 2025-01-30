6f4a024397b24e46a15b1d2902fb13d386b25bc4
Arnau Mas <arnau.mas@outlook.com>
2025-01-30 10:57:57 +0100

    move to packer.nvim

--- nvim/ftplugin/tex.vim
+++
@@ -1,8 +0,0 @@
-"Vim configuration for tex files
-
-" -------- VISUAL STUFF
-"
-"  wrap lines around words
-set wrap linebreak
-
-
--- nvim/link.zsh
+++ nvim/link.zsh
@@ -3,10 +3,5 @@
 # Symlink configuration files
 ln -s "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
-mkdir -p "$HOME/.config/nvim/plugin"
-ln -s "$HOME/dotfiles/nvim/plugin/vimtex.vim" "$HOME/.config/nvim/plugin/vimtex.vim"
-
-mkdir -p "$HOME/.config/nvim/ftplugin"
-
-mkdir -p "$HOME/.config/nvim/luasnip"
-ln -s "$HOME/dotfiles/nvim/luasnip/tex.lua" "$HOME/.config/nvim/luasnip/tex.lua"
+mkdir -p "$HOME/.config/nvim/lua"
+ln -s "$HOME/dotfiles/nvim/lua/packer.lua" "$HOME/.config/nvim/lua/packer.lua"
