5e87f43293404a02c60f08cef4d0620a98c4f817
Arnau Mas <arnau.mas@outlook.com>
2025-02-03 19:32:18 +0100

    move to edge colorscheme

--- nvim/lua/arnau/lazy/colors.lua
+++ nvim/lua/arnau/lazy/colors.lua
@@ -1,17 +1,34 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#a3b1bf" })
-	end,
-})
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+	-- pattern = "*",
+	-- callback = function()
+	-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- 	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#a3b1bf" })
+	-- end,
+-- })
 
 return {
 	{
 		"lunacookies/vim-colors-xcode",
+	},
+
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		variant = "dawn",
+		disable_background = true
+	},
+
+	{
+		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim"
+	},
+	
+	{
+		"sainnhe/edge",
 		config = function()
-			vim.cmd.colorscheme("xcode")
+			vim.cmd.colorscheme('edge')
+			vim.g.edge_transparent_background = 2
 		end
 	}
 }
