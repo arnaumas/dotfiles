42ebc71ded7fa644de64fc5cc7797a0442be5b3e
Arnau Mas <arnau.mas@outlook.com>
2025-03-23 00:39:05 +0100

    install luasnip

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,5 +1,9 @@
 return {
 	'L3MON4D3/LuaSnip',
 	enable_autosnippets = true, 
-	store_selection_keys = "<Tab>",
+	store_selection_keys = '<Tab>',
+
+	config = function()
+		require('luasnip.loaders.from_lua').lazy_load({paths = '~/.config/nvim/snippets/'})
+	end,
 }
