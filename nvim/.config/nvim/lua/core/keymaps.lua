5cb595eb1b8053ef3917102483d3984f9d67b634
Arnau Mas <arnau.mas@outlook.com>
2025-03-23 00:38:33 +0100

    set keybindings for luasnip

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = ' '
-
 local map = vim.keymap.set
 
 map('n', 'o', 'o<esc>', { desc = '[o]pen line' })
@@ -11,3 +9,12 @@ map('n', '<C-j>', '<C-w><C-j>', { desc = 'move focus to the window below' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'move focus to the window above' })
 
 map('n', '<esc>', '<cmd>nohlsearch<cr>', { desc = 'clear highlights' })
+
+map('n', '<leader>pv', vim.cmd.Ex, { desc = '[p]roject [v]iew'})
+
+-- luasnip expand snippet keymaps
+local luasnip = require 'luasnip'
+set.('i', '<C-e>', function() luasnip.expand() end, { silent = true, desc = '[e]xpand snippet' })
+set.({'i', 's'}, '<C-l>', function() luasnip.jump(1) end, { silent = true })
+set.({'i', 's'}, '<C-h>', function() luasnip.jump(-1) end, { silent = true })
+
--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,19 +1,20 @@
 local set = vim.opt
 
-set.number = true --enable line numbers numbers
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-set.splitbelow = true --split new buffers below existing one
-set.splitright = true --vsplit new buffers to the right of existing one
+set.splitbelow = true -- split new buffers below existing one
+set.splitright = true -- vsplit new buffers to the right of existing one
 
-set.linebreak = true --wrap lines automatically
-set.breakindent = true --maintain indentation level of wrapped lines
-
-set.cursorline = true --highlight the line the cursor is one
-set.scrolloff = 20 --maintain 20 lines above and below the cursor
-set.smoothscroll = true --enable smoothscrolling
+set.number = true -- enable line numbers
+set.linebreak = true -- wrap lines automatically
+set.breakindent = true -- maintain indentation level of wrapped lines
+set.cursorline = true -- highlight the line the cursor is one
+set.scrolloff = 20 -- maintain 20 lines above and below the cursor
+set.smoothscroll = true -- enable smoothscrolling
 
 vim.g.have_nerd_font = true
 
 vim.schedule(function()
-	set.clipboard = 'unnamedplus' --sync neovim and system clipboards
+	set.clipboard = 'unnamedplus' -- sync neovim and system clipboards
 end)
