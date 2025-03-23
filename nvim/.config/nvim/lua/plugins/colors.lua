e60c0c7bad062d45d9c5bd0fa09e14d6bdafb563
Arnau Mas <arnau.mas@outlook.com>
2025-03-23 11:31:08 +0100

    change Visual highlight

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,34 +1,42 @@
 -- vim.api.nvim_create_autocmd("ColorScheme", {
 	-- pattern = "*",
 	-- callback = function()
-	-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- 	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#a3b1bf" })
-	-- end,
--- })
+		-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- 	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "#a3b1bf" })
+		-- end,
+		-- })
 
-return {
-	{
-		"lunacookies/vim-colors-xcode",
-	},
+		return {
+			{
+				"lunacookies/vim-colors-xcode",
+			},
 
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		variant = "dawn",
-		disable_background = true
-	},
+			{
+				"rose-pine/neovim",
+				name = "rose-pine",
+				variant = "dawn",
+				disable_background = true
+			},
 
-	{
-		"zenbones-theme/zenbones.nvim",
-		dependencies = "rktjmp/lush.nvim"
-	},
-	
-	{
-		"sainnhe/edge",
-		config = function()
-			vim.cmd.colorscheme('edge')
-			vim.g.edge_transparent_background = 1
-		end
-	}
-}
+			{
+				"zenbones-theme/zenbones.nvim",
+				dependencies = "rktjmp/lush.nvim"
+			},
+
+			{
+				"sainnhe/edge",
+				config = function()
+					vim.g.edge_transparent_background = 1
+					vim.g.edge_enable_italic = true
+					vim.api.nvim_create_autocmd('ColorScheme', {
+						group = vim.api.nvim_create_augroup('custom_highlights_edge', {}),
+						pattern = 'edge',
+						callback = function()
+										vim.api.nvim_set_hl(0, 'Visual', { link = 'IncSearch' })
+						end
+					})
+					vim.cmd.colorscheme('edge')
+				end
+			}
+		}
--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,9 @@
 return {
 	'L3MON4D3/LuaSnip',
-	enable_autosnippets = true, 
-	store_selection_keys = '<Tab>',
+	opts = {
+		enable_autosnippets = true, 
+		store_selection_keys = '<Tab>',
+	},
 
 	config = function()
 		require('luasnip.loaders.from_lua').lazy_load({paths = '~/.config/nvim/snippets/'})
