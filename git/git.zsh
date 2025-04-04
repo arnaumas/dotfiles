89288bd786a010a2e98fdedaf4694b29475330f7
Arnau Mas <arnau.mas@outlook.com>
2025-04-04 17:17:14 +0200

    restructure git config

--- git/git.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# Symlink git configuration files to home directory
-ln -s "$HOME/dotfiles/git/gitconfig" "$HOME/.gitconfig"
-ln -s "$HOME/dotfiles/git/gitignore_global" "$HOME/.gitignore_global"
-
