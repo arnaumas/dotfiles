aa60b31786b16e3dcaf52ff8eb2cb7fe81d4c9fe
Arnau Mas <arnau.mas@outlook.com>
2026-06-25 15:45:13 +0200

    sketchybar: initial sketchybar

---
+++ sketchybar/.config/sketchybar/colors.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# Mirrors colors/light.yaml. Format: 0xAARRGGBB, AA=ff opaque.
+
+export BG=0xffeeedea     
+export HL_BG=0xffdfe0d9 
+export FG=0xff4b505b   
+export DIM=0xffaaa9a4  
+
+export RED=0xffd05858     
+export GREEN=0xff608e32   
+export YELLOW=0xffbe7e05  
+export BLUE=0xff5079be 
+export MAGENTA=0xffb05ccc 
+export CYAN=0xff3a8b84    
+
+export TRANSPARENT=0x00000000
