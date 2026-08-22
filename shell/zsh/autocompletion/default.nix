7e4d72de2d5f664a65092cae289cc1b97ec33349
Arnau Mas <arnau.mas@outlook.com>
2026-08-22 23:17:58 +0200

    rg: make linenumber sreadable

---
+++ shell/zsh/autocompletion/default.nix
@@ -0,0 +1,14 @@
+{ lib, ... }:
+{
+	programs.zsh.autosuggestion = {
+		enable = true;
+		highlight = "fg=7";
+	};
+
+	programs.zsh.localVariables = {
+		ZSH_AUTOSUGGEST_STRATEGY = [ "unique_completion" ];
+		ZSH_AUTOSUGGEST_USE_ASYNC = 1;
+	};
+
+	programs.zsh.initContent = lib.mkAfter (builtins.readFile ./autocompletion.zsh);	
+}
