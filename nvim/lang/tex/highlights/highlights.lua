0c69a841f46a21b444e3426f89ef358450981083
Arnau Mas <arnau.mas@outlook.com>
2026-09-06 01:01:49 +0200

    nvim: add treesitter latex highlights

---
+++ nvim/lang/tex/highlights/highlights.lua
@@ -0,0 +1,12 @@
+---@diagnostic disable: undefined-global
+
+hl('@operator.math', { fg = green })
+link('@markup.math', 'Normal')
+hl('@punctuation.math', { fg = magenta })
+hl('@markup.link.tex', { fg = cyan })
+hl('@markup.link.path', { fg = cyan, underline = true })
+link('@punctuation.backslash', 'Delimiter')
+link('@keyword.import.latex', 'Function')
+link('@keyword.conditional.latex', 'Function')
+link('@module.latex', 'Function')
+link('@label.latex', 'Normal')
---
+++ nvim/lang/tex/highlights/highlights.scm
@@ -0,0 +1,97 @@
+; extends
+
+([
+  (operator)
+  "="
+] @operator.math
+  (#in-math? @operator.math))
+(superscript
+  "^" @operator.math)
+(subscript
+  "_" @operator.math)
+
+(inline_formula
+  [
+    "$"
+    "\\("
+    "\\)"
+  ] @punctuation.math)
+(displayed_equation
+  [
+    "$$"
+    "\\["
+    "\\]"
+  ] @punctuation.math)
+
+((math_environment
+  (begin
+    name: (curly_group_text
+      (text) @punctuation.math)))
+  (#set! "priority" 105))
+((math_environment
+  (end
+    name: (curly_group_text
+      (text) @punctuation.math)))
+  (#set! "priority" 105))
+
+(label_definition
+  name: (curly_group_label
+    (_) @markup.link.tex))
+(label_reference
+  names: (curly_group_label_list
+    (_) @markup.link.tex))
+(label_reference_range
+  from: (curly_group_label
+    (_) @markup.link.tex)
+  to: (curly_group_label
+    (_) @markup.link.tex))
+(citation
+  keys: (curly_group_text_list) @markup.link.tex)
+
+((package_include
+  paths: (curly_group_path_list) @variable.parameter)
+  (#set! "priority" 105))
+((class_include
+  path: (curly_group_path) @variable.parameter)
+  (#set! "priority" 105))
+((latex_include
+  path: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((verbatim_include
+  path: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((import_include
+  directory: (curly_group_path) @markup.link.path
+  file: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((graphics_include
+  path: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((svg_include
+  path: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((inkscape_include
+  path: (curly_group_path) @markup.link.path)
+  (#set! "priority" 105))
+((bibtex_include
+  paths: (curly_group_path_list) @markup.link.path)
+  (#set! "priority" 105))
+((biblatex_include
+  glob: (curly_group_glob_pattern) @markup.link.path)
+  (#set! "priority" 105))
+
+((command_name) @punctuation.backslash
+  (#first-char! @punctuation.backslash)
+  (#set! "priority" 200))
+((_
+  command: _ @punctuation.backslash)
+  (#first-char! @punctuation.backslash)
+  (#set! "priority" 200))
+((math_delimiter
+  left_command: _ @punctuation.backslash)
+  (#first-char! @punctuation.backslash)
+  (#set! "priority" 200))
+((math_delimiter
+  right_command: _ @punctuation.backslash)
+  (#first-char! @punctuation.backslash)
+  (#set! "priority" 200))
