97bbfd4a1366c0c7e0294a5db0c40fd6e5337b2f
Arnau Mas <arnau.mas@outlook.com>
2026-09-06 19:06:41 +0200

    nvim/tex: extract compilation notification autocmds

---
+++ nvim/lang/tex/build.nix
@@ -0,0 +1,39 @@
+{
+  plugins.vimtex = {
+    enable = true;
+    settings = {
+      view_method = "sioyek";
+      view_sioyek_exe = "/Applications/sioyek.app/Contents/MacOS/sioyek";
+      quickfix_open_on_warning = 0;
+      log_ignore = [
+        "Compilation completed"
+        "Compilation failed"
+        "Compiler start"
+        "Compiler stopped"
+      ];
+    };
+  };
+
+  autoCmd = [
+    {
+      event = "User";
+      pattern = "VimtexEventCompileStarted";
+      callback.__raw = "function() vim.notify('VimTeX: compiling ' .. vim.b.vimtex.base, vim.log.levels.INFO) end";
+    }
+    {
+      event = "User";
+      pattern = "VimtexEventCompileSuccess";
+      callback.__raw = "function() vim.notify('VimTeX: compiled ' .. vim.b.vimtex.base, vim.log.levels.INFO) end";
+    }
+    {
+      event = "User";
+      pattern = "VimtexEventCompileStopped";
+      callback.__raw = "function() vim.notify('VimTeX: compiler stopped', vim.log.levels.INFO) end";
+    }
+    {
+      event = "User";
+      pattern = "VimtexEventCompileFailed";
+      callback.__raw = "function() vim.notify(('VimTeX: failed (%d qf entries)'):format(#vim.fn.getqflist()), vim.log.levels.ERROR) end";
+    }
+  ];
+}
