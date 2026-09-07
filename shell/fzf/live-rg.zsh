402e4a601b1194492ca132cd3eac8a091d7a4cb8
Arnau Mas <arnau.mas@outlook.com>
2026-09-07 22:49:41 +0200

    fzf: add live-rg widget

--- shell/fzf/live-rg.zsh
+++ shell/fzf/live-rg.zsh
@@ -1,34 +1,35 @@
-rg-widget() {
+live-rg() {
 	# rg to use
 	local rg_prefix='rg --line-number --no-heading --color=always --smart-case'
 	local out
 	# fzf settings
 	out=$(
 		FZF_DEFAULT_COMMAND= \
-		fzf --ansi --disabled --multi --height=80% \
+		fzf --ansi --disabled --multi \
 			--delimiter : \
+			--prompt 'rg > ' \
 			--bind "change:reload:sleep 0.05; ${rg_prefix} -- {q} || true" \
-	) || { zle reset-prompt; return }
+			--height=30
+	)
 
-	[[ -z $out ]] && { zle reset-prompt; return }
+	# nothing picked
+	[[ -z $out ]] && return
 
 	# parse the matches into editor args
 	local -a args
-	local line file ln first=1
+	local line file ln
 	while IFS= read -r line; do
 		file=${line%%:*}
 		ln=${${line#*:}%%:*}
-		if (( first )); then
-			args=(+$ln -- $file); first=0
-		else
-			args+=(-c "edit +$ln ${(q)file}")
-		fi
+		args+=(-c "edit +$ln ${(q)file}")
 	done <<< "$out"
-	(( ! first )) && args+=(-c 'buffer 1')
+	args+=(-c 'buffer 1')
 
 	# open the matches
-	${EDITOR:-nvim} "${args[@]}" </dev/tty
-	zle reset-prompt
+	BUFFER="${EDITOR:-nvim} ${(j: :)${(@q)args}}"
+	zle accept-line
 }
-zle -N rg-widget
-bindkey '^g' rg-widget
+
+live-rg-widget() { live-rg; zle reset-prompt }
+zle -N live-rg-widget
+bindkey '^g' live-rg-widget
