eedd72c9016645613c223c38feda2b750173956d
Arnau Mas <arnau.mas@outlook.com>
2026-09-24 19:33:27 +0200

    wezterm: add colors to last-output in less

--- wezterm/buffer/last-output/last-output.lua
+++ wezterm/buffer/last-output/last-output.lua
@@ -1,13 +1,36 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+local function escapes(pane, zone)
+	local dims = pane:get_dimensions()
+	local bottom = dims.physical_top + dims.viewport_rows - 1
+	local want = zone.end_y - zone.start_y + 1
+	local lines = {}
+	for line in (pane:get_lines_as_escapes(bottom - zone.start_y + 1) .. '\n'):gmatch '(.-)\n' do
+		if #lines == want then
+			break
+		end
+		lines[#lines + 1] = line
+	end
+	return table.concat(lines, '\n') .. '\n'
+end
+
+local function last_output(pane)
+	local zones = pane:get_semantic_zones 'Output'
+	local y = pane:get_cursor_position().y
+	for i = #zones, 1, -1 do
+		if zones[i].start_y < y then
+			return zones[i]
+		end
+	end
+end
+
 wezterm.on('last-output', function(win, pane)
 	local proc = pane:get_foreground_process_info()
 	if not (proc and proc.name:match 'zsh$') then
 		return
 	end
-	local zones = pane:get_semantic_zones 'Output'
-	local zone = zones[#zones]
+	local zone = last_output(pane)
 	if not zone then
 		return
 	end
@@ -15,7 +38,7 @@ wezterm.on('last-output', function(win, pane)
 	if not f then
 		return
 	end
-	f:write(pane:get_text_from_semantic_zone(zone))
+	f:write(escapes(pane, zone))
 	f:close()
 	win:perform_action(act.SendString '\x1b@seq@', pane)
 end)
