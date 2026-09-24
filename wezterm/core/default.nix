2168c4e9d0a0ad9f851889a12a6855f0ced0a756
Arnau Mas <arnau.mas@outlook.com>
2026-09-24 11:37:46 +0200

    wezterm: split core into a folder

---
+++ wezterm/core/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./keys.nix
+    ./theme.nix
+    ./window.nix
+  ];
+}
--- wezterm/default.nix
+++ wezterm/default.nix
@@ -1,8 +1,6 @@
 {
   imports = [
-    ./core.nix
+    ./core
     ./splits
-    ./theme.nix
-    ./ui.nix
   ];
 }
