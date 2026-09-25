6590ba9bf77f071e76ff26f2ada59e8eb58833a0
Arnau Mas <arnau.mas@outlook.com>
2026-09-25 02:43:21 +0200

    shell: make a better ll alias

--- shell/core.nix
+++ shell/core.nix
@@ -10,6 +10,7 @@
       MANWIDTH = "999";
       PYTHON_HISTORY = "${config.xdg.stateHome}/python/history";
       CLAUDE_CONFIG_DIR = "${config.xdg.configHome}/claude";
+    	GNUPGHOME="${config.xdg.configHome}/gnupg";
       LS_COLORS = "rs=0:no=0:fi=0:di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43";
     };
 
@@ -19,11 +20,13 @@
       rm = "rm -Ivr";
       python = "python3 -q";
       vim = "nvim";
-      ll = "ls -ohAF --color=always | sort -k1r | sed '1d;/.DS_Store/d;s/^.\\{11\\}[[:space:]]*[[:digit:]]*[[:space:]]//g'";
     };
   };
-
-  programs.zsh.envExtra = ''
-    		export GNUPGHOME="${config.xdg.configHome}/gnupg"
-    	'';
+  
+  programs.zsh.initContent = ''
+    ll() {
+      LC_COLLATE=C ls -ohAF --color=always --group-directories-first "$@" \
+      | sed '1d;/.DS_Store/d;s/^[^ ]*[[:space:]]*[[:digit:]]*[[:space:]]//'
+    }
+  '';
 }
